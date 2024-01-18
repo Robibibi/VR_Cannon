2bd7c9c1ae4b061dec86fdb5872cdd70499e3803
Robin Lotode <lotode.rob@gmail.com>
2024-01-18 09:55:49 +0100

    horizontal rotate done, added xr inputs to gitignore

--- Assets/Scripts/RotateCannon.cs
+++ Assets/Scripts/RotateCannon.cs
@@ -10,43 +10,55 @@ public class RotateCannon : MonoBehaviour
     public Transform leftController;
     public float rotateSpeed, thresholdAngle;
     private XRSimpleInteractable interactable;
-    private Vector3 grabDirection, controllerDirection;
+    private Vector3 grabDirection, controllerDirection, cannonDirection;
+    private Quaternion currentRot;
+    private Vector3 startPos;
+    private bool offsetSet;
 
     // Start is called before the first frame update
     void Start()
     {
         interactable = GetComponent<XRSimpleInteractable>();
         interactable.selectEntered.AddListener(OnSelectEnter);
+        offsetSet = false;
     }
 
     void OnSelectEnter(SelectEnterEventArgs args)
     {
-        grabDirection = args.interactableObject.transform.position - args.interactorObject.transform.position;
-        print("grab dir : " + grabDirection);
-        print("controller dir : " + args.interactorObject.transform.forward);
+        // grabDirection = args.interactableObject.transform.position - args.interactorObject.transform.position;
+        
+        // print("grab dir : " + grabDirection);
+        // print("controller dir : " + args.interactorObject.transform.forward);
+    }
+
+    void SetOffsets()
+    {
+        if (offsetSet)
+            return;
+        startPos = Vector3.Normalize(leftController.transform.position - transform.position);
+        currentRot = rotator.transform.rotation;
+        print("start point : " + startPos);
+ 
+        offsetSet = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(interactable.isSelected) {
+            SetOffsets();
             controllerDirection = leftController.transform.forward;
-            // print("grab dir : " + grabDirection);
-            // print("controller dir : " + controllerDirection);
-            // print("angle : " + Vector3.Angle(grabDirection, controllerDirection));
-            Vector3 newDir = leftController.transform.InverseTransformDirection(grabDirection).normalized;
-            print("newDir : " + newDir);
-            print(Math.Abs(newDir.x));
-
-
-            if (Math.Abs(newDir.x) > thresholdAngle) {
-                print("rot " + newDir.x * rotateSpeed);
-                rotator.transform.Rotate(new Vector3(0, 1, 0), newDir.x * rotateSpeed);
-                
-            }
-            // print("Selected");
+            cannonDirection = rotator.transform.forward;
+            Vector3 closestPoint = Vector3.Normalize(leftController.transform.position - transform.position);
+            // print("closest point : " + closestPoint);
+            var rot = Quaternion.FromToRotation(startPos, closestPoint);
+            rot = Quaternion.Euler(0, rot.eulerAngles.y, 0);
+            rotator.transform.rotation = rot * currentRot;
+            // print("rotating " + rotator.transform.rotation);
         }
-
+        else {
+            offsetSet = false;
+        }
         
     }
 }
